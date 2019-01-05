d589d1966b047cdbfe393349e16fa23b38454b7a
Eduardo Neves <neves98@outlook.com>
2019-01-05 10:49:24 -0200

    product: criando endpoints de produto

---
+++ app/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Promotion;
+use Illuminate\Http\Request;
+
+class Product extends Model
+{
+    use SoftDeletes;
+	
+    public $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'prescription'
+    ];
+
+    public $timestamps = true;
+
+    public function promotions()
+    {
+        return $this->belongsTo(Promotion::class, 'promotion_id', 'id');
+    }
+
+    public function scopeSearch($query, Request $request) 
+    {
+        if ($request->has('name')) {
+            $query->where('name', 'LIKE', '%'.strtolower($request->input('name')).'%');
+        }
+
+        if ($request->has('prescription')) {
+            $query->where('prescription', 'LIKE', '%'.strtolower($request->input('prescription')).'%');
+        }
+
+        if ($request->has('limit') && !empty($request->input('limit'))) {
+            $query->limit($request->input('limit'));
+        }
+
+        if ($request->has('offset') && !empty($request->input('offset'))) {
+            $query->offset($request->input('offset'));
+        }
+
+        return $query->with('promotions')->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'UserController@login');
 
+Route::get('products/search', 'ProductController@search');
+
 // Todas as rotas devem estar aqui
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('register', 'UserController@register');
 
+    Route::resource('products', 'ProductController');
 });
